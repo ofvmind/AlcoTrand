567b25b480d63365a0bf2753382f87c29b4deb1e
ofvmind <120343660+ofvmind@users.noreply.github.com>
2023-11-07 05:30:07 +0200

    update music

--- src/components/places/places.js
+++ src/components/places/places.js
@@ -14,8 +14,10 @@ import PorterAkadem from "../../images/places/PorterAkademgorodok.jpg";
 import musicTest from "../../music/places/test.mp3";
 import musicBorschagovka from "../../music/places/Borschagovka.mp3";
 import musicVyshnya from "../../music/places/Vyshnya.mp3";
+import musicFc from "../../music/places/fc.mp3";
 import musicVidradniy from "../../music/places/Vidradniy.mp3";
-import PorterAkademgorodok from "../../music/places/PorterAkadem.mp3";
+import musicPorterAkademgorodok from "../../music/places/PorterAkadem.mp3";
+import musicPolyana from "../../music/places/Polyana.mp3";
 
 export const drinkSets_heavy = [
   "Джин",
@@ -43,11 +45,11 @@ export const places = [
   { id: 1, place: "Борщагівка", image: borschagovka, music: musicBorschagovka },
   { id: 2, place: "Відрадний", image: vidredniy, music: musicVidradniy },
   { id: 3, place: "Вишневе", image: vyshneve, music: musicVyshnya },
-  { id: 4, place: "Легенда ФК", image: FC, music: musicTest },
+  { id: 4, place: "Легенда ФК", image: FC, music: musicFc },
   { id: 5, place: "Центр", image: Chreschatik, music: musicTest },
   { id: 6, place: "Парк Совки", image: Sovki, music: musicTest },
   { id: 7, place: "ГідроПарк", image: Gidropark, music: musicTest },
-  { id: 8, place: "Поляна", image: Polyana, music: musicTest },
+  { id: 8, place: "Поляна", image: Polyana, music: musicPolyana },
   { id: 9, place: "Бар79", image: TayneMisce, music: musicTest },
-  { id: 10, place: "Портер", image: PorterAkadem, music: PorterAkademgorodok }
+  { id: 10, place: "Портер", image: PorterAkadem, music: musicPorterAkademgorodok }
 ];
