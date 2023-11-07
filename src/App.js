5f2d46d552aa81414f6ffb92240635d0ca0c713a
ofvmind <120343660+ofvmind@users.noreply.github.com>
2023-11-07 03:08:27 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,3 @@
-import React, { useState } from "react";
-import { drinkSets, places } from "./components/places/places";
-import music from "./music/loading.mp3";
-import { StartButton } from "./components/UI/StartButton/StartButton";
-import { Loader } from "./components/UI/Loader/Loader";
 import { Main } from "./components/app-component/Main";
 
 function App() {
