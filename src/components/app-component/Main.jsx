24511b72e3a43b44f26bd7524afd57c55b7178c9
ofvmind <120343660+ofvmind@users.noreply.github.com>
2023-12-07 15:00:04 +0200

    fix data of arrays

--- src/components/app-component/Main.jsx
+++ src/components/app-component/Main.jsx
@@ -8,119 +8,13 @@ import { Modal } from "../UI/Modal/Modal";
 import { OptionsPage } from "../AppRouter/options-page/OptionsPage";
 import { AddPlace } from "../AppRouter/options-page/AddPlace";
 import { ChangePlaces } from "../AppRouter/options-page/ChangePlaces";
-
-//Місця
-import borschagovka from "../../images/places/borschagovka.jpg";
-import vidredniy from "../../images/places/otradniy.jpg";
-import vyshneve from "../../images/places/vyshneve.jpg";
-import FC from "../../images/places/FC.jpg";
-import Chreschatik from "../../images/places/Chreschatik.jpg";
-import Sovki from "../../images/places/SquareSovki.jpg";
-import Gidropark from "../../images/places/Gidropark.png";
-import Polyana from "../../images/places/Polyana.jpeg";
-import TayneMisce from "../../images/places/BarVernadskogo79.jpg";
-import PorterAkadem from "../../images/places/PorterAkademgorodok.jpg";
-
-//Музика
-import musicTest from "../../music/places/test.mp3";
-import musicBorschagovka from "../../music/places/Borschagovka.mp3";
-import musicVyshnya from "../../music/places/Vyshnya.mp3";
-import musicFc from "../../music/places/fc.mp3";
-import musicVidradniy from "../../music/places/Vidradniy.mp3";
-import musicPorterAkademgorodok from "../../music/places/PorterAkadem.mp3";
-import musicPolyana from "../../music/places/Polyana.mp3";
+import { alcoData, placesData } from "../places/places";
 import { AlcoOptions } from "../AppRouter/options-page/AlcoOption";
 
 export const Main = () => {
-  const [places, setPlaces] = useState([
-    {
-      id: 1,
-      place: "Борщагівка",
-      image: borschagovka,
-      music: musicBorschagovka,
-      included: true,
-    },
-    {
-      id: 2,
-      place: "Відрадний",
-      image: vidredniy,
-      music: musicVidradniy,
-      included: true,
-    },
-    {
-      id: 3,
-      place: "Вишневе",
-      image: vyshneve,
-      music: musicVyshnya,
-      included: true,
-    },
-    {
-      id: 4,
-      place: "Парк Совки",
-      image: Sovki,
-      music: musicTest,
-      included: true,
-    },
-    { id: 5, place: "Легенда ФК", image: FC, music: musicFc, included: true },
-    {
-      id: 6,
-      place: "Портер",
-      image: PorterAkadem,
-      music: musicPorterAkademgorodok,
-      included: true,
-    },
-    {
-      id: 7,
-      place: "Бар79",
-      image: TayneMisce,
-      music: musicTest,
-      included: true,
-    },
-    {
-      id: 8,
-      place: "Центр",
-      image: Chreschatik,
-      music: musicTest,
-      included: true,
-    },
-    {
-      id: 9,
-      place: "ГідроПарк",
-      image: Gidropark,
-      music: musicTest,
-      included: true,
-    },
-    {
-      id: 10,
-      place: "Поляна",
-      image: Polyana,
-      music: musicPolyana,
-      included: true,
-    },
-  ]);
+  const [places, setPlaces] = useState([...placesData]);
 
-  const [alco, setAlco] = useState([
-    { id: 1, name: "Пиво", strength: "light", included: true },
-    { id: 2, name: "Шоти", strength: "light", included: true },
-    { id: 3, name: "Shake", strength: "light", included: true },
-    { id: 4, name: "Вино", strength: "light", included: true },
-    { id: 5, name: "Сидр", strength: "light", included: true },
-    { id: 6, name: "Джин Тонік", strength: "light", included: true },
-    { id: 7, name: "Крепе пиво", strength: "light", included: true },
-    { id: 8, name: "Ель", strength: "light", included: true },
-    { id: 9, name: "Шейк", strength: "light", included: true },
-    { id: 10, name: "Лїкер", strength: "light", included: true },
-    { id: 11, name: "Джин", strength: "light", included: true },
-    { id: 12, name: "Зубровка", strength: "hard", included: true },
-    { id: 13, name: "Revo", strength: "hard", included: true },
-    { id: 14, name: "Самбука", strength: "hard", included: true },
-    { id: 15, name: "Водка", strength: "hard", included: true },
-    { id: 16, name: "Абсент", strength: "hard", included: true },
-    { id: 17, name: "Віскі", strength: "hard", included: true },
-    { id: 18, name: "Скотч", strength: "hard", included: true },
-    { id: 19, name: "Самогон", strength: "hard", included: true },
-    { id: 20, name: "Коньяк", strength: "hard", included: true },
-  ]);
+  const [alco, setAlco] = useState([...alcoData]);
 
   const [picture, setPicture] = useState(places[9]);
   const [clicked, setClicked] = useState(false);
